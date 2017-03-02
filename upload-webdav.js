5250232fb21c471cac2a0b7276c3a4e81858f558
Stanislav German <s.german@rambler-co.ru>
2017-03-02 18:28:47 +0300

    add wabdav-fs and scaffold init task

--- upload-webdav.js
+++ upload-webdav.js
@@ -1,17 +1,24 @@
 #!/usr/bin/env node
 const prog = require('caporal');
+const webdav_fs = require("webdav-fs")
 
 prog
   .version('1.0.0')
+  .command('init', 'initialize webdav credentials')
+  .action(() => {
+    console.log('initializing...')
+  })
   .command('deploy', 'Deploy an application')
+  .argument('[env]', 'Environment to deploy on', /^s1|s2|s3|s4|production$/, 's1')
+  .argument('[env]', 'Environment to deploy on', /^s1|s2|s3|s4|production$/, 's1')
   .argument('[env]', 'Environment to deploy on', /^s1|s2|s3|s4|production$/, 's1')
   .action(function(args, options, logger) {
     console.log(args)
     // args and options are objects
     // args = {"app": "myapp", "env": "production"}
     // options = {"tail" : 100}
-  });
+  })
 
 prog.parse(process.argv);
 
-// ./myprog deploy myapp production --tail 100
+// ./upload-webdav deploy production
