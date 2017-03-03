92274758ace5b14d2e834c21eca99f28d721eb88
Stanislav German <s.german@rambler-co.ru>
2017-03-03 13:01:30 +0300

    write default params and validate config path

--- upload-webdav.js
+++ upload-webdav.js
@@ -1,32 +1,29 @@
 #!/usr/bin/env node
-const prog = require('caporal');
+const prog = require('caporal')
 const webdav_fs = require('webdav-fs')
 const jsonfile = require('jsonfile')
+
+const config_path_validator = /^.*.json$/
 
 prog
   .version('1.0.0')
   .command('init', 'initialize webdav credentials')
-  .argument('[config]', 'path for config', /^s1|s2|s3|s4|production$/, 's1')
-  .action(() => {
-    const file_path = 'upload-webdav.json'
+  .argument('[config]', 'path for config json', config_path_validator, 'upload-webdav.json')
+  .action((args) => {
     const default_config = {
       uri: 'http://path_to_webdav',
       username: 'username',
       password: 'password'
     }
-    jsonfile.writeFile(file_path, default_config, function (err) {
+    jsonfile.writeFile(args['config'], default_config, function (err) {
       console.error(err)
     })
   })
   .command('deploy', 'Deploy an application')
-  .argument('[env]', 'Environment to deploy on', /^s1|s2|s3|s4|production$/, 's1')
-  .argument('[env]', 'Environment to deploy on', /^s1|s2|s3|s4|production$/, 's1')
+  .argument('[config]', 'path to config', null, 'upload-webdav.json')
   .argument('[env]', 'Environment to deploy on', /^s1|s2|s3|s4|production$/, 's1')
   .action(function(args, options, logger) {
     console.log(args)
-    // args and options are objects
-    // args = {"app": "myapp", "env": "production"}
-    // options = {"tail" : 100}
   })
 
 prog.parse(process.argv);
